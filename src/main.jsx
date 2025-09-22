d22b56945c1251cd39a150776836cf96422152c9
Felipe <oyolafelipe89@gmail.com>
2025-09-22 09:23:27 -0500

    lili

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// Importar favicon desde src/assets/images
+import lgtaIcon from './assets/images/lgta.png';
+
+// Crear y agregar el favicon dinámicamente
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = lgtaIcon;
+document.head.appendChild(link);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
